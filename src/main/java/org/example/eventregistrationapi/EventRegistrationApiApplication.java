4c6f592d2098ee65548343c28e35212206f2278e
ghusharib najam <fa23-bcs-054@cuilahore.edu.pk>
2026-06-12 19:21:11 +0500

    Started with main file

---
+++ src/main/java/org/example/eventregistrationapi/EventRegistrationApiApplication.java
@@ -0,0 +1,12 @@
+package org.example.eventregistrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegistrationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventRegistrationApiApplication.class, args);
+    }
+}
